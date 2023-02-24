314d63af6ce85a2590b83d1922df27dc32538858
SilentChaos <avanishm471@gmail.com>
2023-02-24 18:50:20 +0530

    dep

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ function sadata(){
   let listItems="" 
 for (let i = 0; i < myLeads.length; i++) {
     listItems+= `<li>
-    <a target='_blank' href='${myLeads[i]}'>" ${myLeads[i]} "</a>
-    </li>" `
+    <a target='_blank' href='${myLeads[i]}'>${myLeads[i]} </a>
+    </li> `
 }
 saved.innerHTML=listItems
 }
