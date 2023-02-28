09af8ae1fe2cb4099fa2e8638e6c2c483eefb05e
SilentChaos <avanishm471@gmail.com>
2023-02-28 15:47:01 +0530

    parameters

--- index.js
+++ index.js
@@ -4,24 +4,34 @@ let saved = document.getElementById("ull")
 let listItems=""
 const inputEl = document.getElementById("text-el")
 const saveBtn = document.getElementById("input-btn")
-localStorage.clear()
-
+const delEl=document.getElementById("del-btn")
 let leadsFromLocalStorage=JSON.parse(localStorage.getItem("MyLeads"))
-console.log(leadsFromLocalStorage)
+if(leadsFromLocalStorage){
+  myLeads=leadsFromLocalStorage
+  sadata(myLeads)
+}
+function sadata(leads){ 
+  let listItems="" 
+for (let i = 0; i < leads.length; i++) {
+    listItems+= `<li>
+    <a target='_blank' href='${leads[i]}'>${leads[i]} </a>
+    </li> `
+}
+saved.innerHTML=listItems
+}
+
+delEl.addEventListener('dblclick',function(){
+  localStorage.clear()
+  myLeads=[]
+  sadata(myLeads) 
+})
+
+
 saveBtn.addEventListener('click', function() {
   myLeads.push(inputEl.value)
   inputEl.value=""
 localStorage.setItem("MyLeads",JSON.stringify(myLeads))
   
-sadata()
+sadata(myLeads)
 console.log(localStorage.getItem("MyLeads"))
 })
-function sadata(){ 
-  let listItems="" 
-for (let i = 0; i < myLeads.length; i++) {
-    listItems+= `<li>
-    <a target='_blank' href='${myLeads[i]}'>${myLeads[i]} </a>
-    </li> `
-}
-saved.innerHTML=listItems
-}
